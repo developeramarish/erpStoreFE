91b26b8ffbcfcf1b16e5e181ebb7dfe06bb7fe52
Angular CLI <angular-cli@angular.io>
2018-02-02 20:53:48 -0500

    chore: initial commit from @angular/cli

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('itcusco-root h1')).getText();
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'itcusco-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'itcusco';
+}
